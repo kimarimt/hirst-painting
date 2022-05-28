e6711c531f3ef00049a8e0e21a0422fae04b375c
Alec German <kimarimt@protonmail.com>
2022-05-28 12:58:06 -0400

    built the structure of the hirst painting maker

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import turtle
+
+
 def main():
-    print('Hello, World!')
+    s = turtle.getscreen()
+    t = turtle.Turtle()
+
+    y = -300
+
+    t.hideturtle()
+    t.penup()
+    t.goto(-325, y)
+    t.shape('circle')
+    t.fillcolor('red')
+    t.pencolor('red')
+
+    while t.ycor() < 340:
+        t.stamp()
+
+        while t.xcor() < 315:
+            t.forward(40)
+            t.stamp()
+
+        y += 40
+        t.goto(-325, y)
+
+    s.exitonclick()
 
 
 if __name__ == '__main__':
