33f5422becf64800f3df0e0ad6af3bc829dfc4ee
Alec German <kimarimt@protonmail.com>
2022-05-28 13:23:39 -0400

    added colorgram.py to get colors from hirst.jpg to use for painting

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import turtle
+import random
+import colorgram
+
+
+def change_color(object, colors):
+    color = random.choice(colors)
+    code = color.rgb
+    object.fillcolor(code.r, code.g, code.b)
+    object.pencolor(code.r, code.g, code.b)
 
 
 def main():
+    colors =  colorgram.extract('hirst.jpg', 50)
     s = turtle.getscreen()
+    s.colormode(255)
     t = turtle.Turtle()
 
     y = -300
@@ -11,13 +22,14 @@ def main():
     t.penup()
     t.goto(-325, y)
     t.shape('circle')
-    t.fillcolor('red')
-    t.pencolor('red')
+
 
     while t.ycor() < 340:
+        change_color(t, colors)
         t.stamp()
 
         while t.xcor() < 315:
+            change_color(t, colors)
             t.forward(40)
             t.stamp()
 
